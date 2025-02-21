3389f6927865b1dc92b1a849d6bac3971918ef06
JakubPiech <piechjakub99@gmail.com>
2025-02-21 02:02:06 +0100

    logging added to traffic generator

--- server/mock/mqtt_spam.py
+++ server/mock/mqtt_spam.py
@@ -6,6 +6,9 @@ import aiomqtt
 
 from server.core import KEONN_BROKER_CONF
 from server.models import Device, Location, Tag
+from server.logging import get_configured_logger
+
+logger = get_configured_logger("server.mock", level="DEBUG")
 
 
 async def generate_mqtt_events():
@@ -14,16 +17,21 @@ async def generate_mqtt_events():
     async with aiomqtt.Client(
         **KEONN_BROKER_CONF, identifier=send_dev.meta.id
     ) as client:
+        logger.info(
+            f"Connected to broker at {KEONN_BROKER_CONF['hostname']} as {send_dev.meta.id}"
+        )
         try:
             locations: list[str] = await Location.filter(device=send_dev).values_list(
                 "loc", flat=True
             )
             s_locs = random.sample(locations, min(3, len(locations)))
+            logger.debug(f"Selected locations: {s_locs}")
             tags: list[int] = await Tag.filter(last_loc_seen_id__in=s_locs).values_list(
                 "id", flat=True
             )
             s_tags = random.sample(tags, 10)
             epcs = await Tag.filter(id__in=s_tags).values_list("epc", flat=True)
+            logger.debug(f"Selected tags: {epcs}")
 
             s_locs = [*(loc.split("/", 1)[1] for loc in s_locs), None]
             last_mutation_time = 0
@@ -32,6 +40,7 @@ async def generate_mqtt_events():
             while True:
                 if time.time() - last_mutation_time > 10:
                     last_mutation_time = time.time()
+                    logger.info("Mutating tags!")
                     for epc in random.sample(epcs, 3):
                         state[epc] = random.choice(s_locs)
 
@@ -43,11 +52,12 @@ async def generate_mqtt_events():
                     message += f"|{epc}:{rssi}@{state[epc]}"
                 if message:
                     await client.publish("RFID/devices", send_dev.mac + message)
+                    logger.debug(f"Published: {send_dev.mac + message}")
                 await asyncio.sleep(3)
         except asyncio.CancelledError:
             raise asyncio.CancelledError
         except Exception as e:
-            print(e)
+            logger.exception(str(e))
 
 
 if __name__ == "__main__":
