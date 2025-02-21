6426cdb4e39254b47eab7b38c4c7cb7ebda8d1db
JakubPiech <piechjakub99@gmail.com>
2025-02-21 01:00:03 +0100

    parser error handling and logging added

--- server/core/parser.py
+++ server/core/parser.py
@@ -3,7 +3,7 @@ from collections.abc import AsyncGenerator
 from dataclasses import dataclass
 
 from aiomqtt.client import Client
-
+from ..logging import get_configured_logger
 # sample message content "60:e8:5b:0a:78:5f|e28011700000020f7cbd7358:-39@1/0/0|e28011700000020f7cbd73a7:-39@1/0/0|00000000deadbeef:-31@1/0/0|e28011700000020f7cbd7348:-41@1/0/0|e28011700000020f7cbd73c7:-46@1/0/0"
 
 
@@ -14,14 +14,20 @@ class ReadEvent:
     location: str  # 60:e8:5b:0a:78:5f/1/0/0
 
 
+logger = get_configured_logger("server.parser")
+
+
 async def keonn_revents_stream(
     mesidz_stream: Client.MessagesIterator,
 ) -> AsyncGenerator[list[ReadEvent], None]:
     async for message in mesidz_stream:
-        message = message.payload.decode()
-        mac, *tags = message.split("|")
-        revents: list[ReadEvent] = []
-        for tag in tags:
-            epc, rssi, location = re.split(":|@", tag)
-            revents.append(ReadEvent(epc, int(rssi), f"{mac}/{location}"))
-        yield revents
+        try:
+            message = message.payload.decode()
+            mac, *tags = message.split("|")
+            revents: list[ReadEvent] = []
+            for tag in tags:
+                epc, rssi, location = re.split(":|@", tag)
+                revents.append(ReadEvent(epc, int(rssi), f"{mac}/{location}"))
+            yield revents
+        except Exception as e:
+            logger.error(str(e))
