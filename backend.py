9dad9b3da3bb30429071970b0bd38472cfb30b64
JakubPiech <piechjakub99@gmail.com>
2025-02-21 02:04:16 +0100

    mqtt core bug fixes, error handling added and logging updated

--- backend.py
+++ backend.py
@@ -3,6 +3,7 @@ import logging
 import os
 import sys
 from contextlib import asynccontextmanager
+from server.core.mqtt import monitor_lost, process_kmqtt, event_sink
 from server.mock.mqtt_spam import generate_mqtt_events
 from server import create_app
 from fastapi import FastAPI
@@ -11,7 +12,9 @@ from fastapi import FastAPI
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     tasks: list[asyncio.Task] = []
+    tasks.append(asyncio.create_task(event_sink()))
     tasks.append(asyncio.create_task(process_kmqtt()))
+    tasks.append(asyncio.create_task(monitor_lost()))
     tasks.append(asyncio.create_task(generate_mqtt_events()))
 
     yield
@@ -26,7 +29,6 @@ app = create_app(lifespan=lifespan)
 if __name__ == "__main__":
     import uvicorn
 
-    logging.basicConfig(level=logging.DEBUG)
     if sys.platform.lower() == "win32" or os.name.lower() == "nt":
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
     uvicorn.run(app, host="0.0.0.0", port=5000)
--- server/core/mqtt.py
+++ server/core/mqtt.py
@@ -1,5 +1,4 @@
 import asyncio
-import logging
 from contextlib import suppress
 from datetime import datetime, timedelta, timezone
 
@@ -7,6 +6,7 @@ import aiomqtt
 
 from server.core import KEONN_BROKER_CONF, LOST_THRESHOLD
 from server.core.parser import keonn_revents_stream
+from server.logging import get_configured_logger
 from server.models import (
     Event,
     EventType,
@@ -16,9 +16,7 @@ from server.models import (
     TagStatus,
 )
 
-logger = logging.getLogger("iot")
-logger.setLevel(logging.DEBUG)
-
+logger = get_configured_logger("server.mqtt", "WARN")
 
 equeue = asyncio.Queue()
 
@@ -55,64 +53,72 @@ async def process_kmqtt():
         await kmqtt.subscribe("RFID/devices")
 
         async for revents in keonn_revents_stream(kmqtt.messages):
-            tags = (
-                await Tag.filter(epc__in=[e.epc for e in revents])
-                .prefetch_related("last_loc_seen")
-                .all()
-            )
-            locobjs = await Location.filter(id__in=[e.location for e in revents]).all()
+            try:
+                tags = (
+                    await Tag.filter(epc__in=[e.epc for e in revents])
+                    .prefetch_related("last_loc_seen")
+                    .all()
+                )
+                locobjs = await Location.filter(
+                    loc__in=[e.location for e in revents]
+                ).all()
 
-            tevents = []
-            for e in revents:
-                dbtag, mloc = None, None
-                with suppress(StopIteration):
-                    dbtag = next(filter(lambda t: t.epc == e.epc, tags))
-                with suppress(StopIteration):
-                    mloc = next(filter(lambda lo: lo.id == e.location, locobjs))
+                tevents = []
+                for e in revents:
+                    dbtag, mloc = None, None
+                    with suppress(StopIteration):
+                        dbtag = next(filter(lambda t: t.epc == e.epc, tags))
+                    with suppress(StopIteration):
+                        mloc = next(filter(lambda lo: lo.loc == e.location, locobjs))
 
-                if not mloc:
-                    logger.error(f"No object in db for location '{e.location}'!")
+                    if not mloc:
+                        logger.error(f"No object in db for location '{e.location}'!")
 
-                if dbtag:
-                    # Tag exists in DB
-                    if dbtag.status == TagStatus.LOST:
-                        tevents.append(
-                            TagEvent(
-                                type=EventType.TAG_REAPPEARED,
-                                tag=dbtag,
-                                data={},
+                    if dbtag:
+                        # Tag exists in DB
+                        if dbtag.status == TagStatus.LOST:
+                            tevents.append(
+                                TagEvent(
+                                    type=EventType.TAG_REAPPEARED,
+                                    tag=dbtag,
+                                    data={},
+                                )
                             )
-                        )
 
-                    if dbtag.last_loc_seen and dbtag.last_loc_seen.id != e.location:
-                        tevents.append(
-                            TagEvent(
-                                type=EventType.TAG_LOC_CHANGE,
-                                tag=dbtag,
-                                data={
-                                    "from": dbtag.last_loc_seen.id,
-                                    "to": e.location,
-                                },
+                        if (
+                            dbtag.last_loc_seen
+                            and dbtag.last_loc_seen.loc != e.location
+                        ):
+                            tevents.append(
+                                TagEvent(
+                                    type=EventType.TAG_LOC_CHANGE,
+                                    tag=dbtag,
+                                    data={
+                                        "from": dbtag.last_loc_seen.loc,
+                                        "to": e.location,
+                                    },
+                                )
                             )
-                        )
 
-                    dbtag.last_loc_seen = mloc
-                    dbtag.last_active_at = datetime.now(timezone.utc)
-                    dbtag.status = TagStatus.ACTIVE
-                    await dbtag.save()
-                else:
-                    # Tag doesnt exist in DB
-                    ntag = await Tag.create(
-                        last_active_at=datetime.now(timezone.utc),
-                        status=TagStatus.ACTIVE,
-                        epc=e.epc,
-                        RSSI=e.RSSI,
-                        last_loc_seen=mloc,
-                        name=e.epc,
-                        description="",
-                    )
-                    tevents.append(
-                        TagEvent(type=EventType.TAG_ADDED, tag=ntag, data={})
-                    )
-            if tevents:
-                await equeue.put(tevents)
+                        dbtag.last_loc_seen = mloc
+                        dbtag.last_active_at = datetime.now(timezone.utc)
+                        dbtag.status = TagStatus.ACTIVE
+                        await dbtag.save()
+                    else:
+                        # Tag doesnt exist in DB
+                        ntag = await Tag.create(
+                            last_active_at=datetime.now(timezone.utc),
+                            status=TagStatus.ACTIVE,
+                            epc=e.epc,
+                            RSSI=e.RSSI,
+                            last_loc_seen=mloc,
+                            name=e.epc,
+                            description="",
+                        )
+                        tevents.append(
+                            TagEvent(type=EventType.TAG_ADDED, tag=ntag, data={})
+                        )
+                if tevents:
+                    await equeue.put(tevents)
+            except Exception as e:
+                logger.exception(str(e))
